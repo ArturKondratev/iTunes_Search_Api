586ef2585f3055f8d3d7fa40e0df7a90aa5e63db
ArturKondratev <a.kondrat473@gmail.com>
2022-12-13 17:34:28 +0300

    task 7.1

---
+++ iOSArchitecturesDemo/Flows/AppDetail/Controller/AppDetailWhatsNewViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AppDetailWhatsNewViewController.swift
+//  iOSArchitecturesDemo
+//
+//  Created by Артур Кондратьев on 11.12.2022.
+//  Copyright © 2022 ekireev. All rights reserved.
+//
+
+import Foundation
---
+++ iOSArchitecturesDemo/Flows/AppDetail/View/AppDetailWhatsNewView.swift
@@ -0,0 +1,21 @@
+//
+//  AppDetailWhatsNewView.swift
+//  iOSArchitecturesDemo
+//
+//  Created by Артур Кондратьев on 11.12.2022.
+//  Copyright © 2022 ekireev. All rights reserved.
+//
+
+import UIKit
+
+class AppDetailWhatsNewView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
